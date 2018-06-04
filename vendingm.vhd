583a604bd3ececd734fc9567efa65a41f0cbf9ea
sashi <sashi_thapaliya@hotmail.com>
2018-06-03 17:13:40 -0700

    Updated with better workflow

--- vendingm.vhd
+++ vendingm.vhd
@@ -3,7 +3,11 @@ use ieee.numeric_std.all;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity vending is
-port (clk,in_dime, in_nickel, rest: in bit;
+port (clk : in bit;
+      in_dime: in bit;
+      in_nickel: in bit;
+      in_quarter: in bit;
+      rest: in bit;
 			num_item: in integer;
 			return_dime: out integer;
 			return_nickel: out integer;
@@ -16,8 +20,8 @@ port (clk,in_dime, in_nickel, rest: in bit;
 end vending;
 
 architecture arch_vending of vending is
-  type state_type is (initial, idle, buying, reset, finish);
-  signal current_s,release_s, next_s, transaction_s: state_type; 
+  type state_type is (initial, selectQ, coins,output,reset);
+  signal current_s, next_s: state_type; 
   signal num_gums_wanted: integer:=0;
   signal deposit_cents: integer:=0;
   signal counter: integer:=0;
@@ -29,20 +33,59 @@ begin
     begin 
       if (rest='1') then
         current_s <= reset;
-      elsif (release_s = idle and current_s = buying) then
-        next_s <= reset;
+      elsif (num_item > 0) then
+        current_s <= selectQ;
       else
-        current_s <= buying;
-        next_s <= buying;
-        counter <= counter+1;
+        current_s <= next_s;
       end if;
 		end process;
 		
-		process(counter, in_dime, in_nickel, next_s, num_item)
+		process(clk, current_s)
 		  begin
-		    num_gums_wanted <= num_item;
-		    if(next_s = reset) then
-		      		message<=0;
+		    if (current_s = selectQ) then
+		      num_gums_wanted <= num_item;
+		      total_price <= 20*num_item;
+		      next_s <= coins;
+		    elsif (current_s = coins) then
+		      if (in_dime='1') then
+		        deposit_cents <= deposit_cents + 5;
+		        display_total_deposit <= deposit_cents+5;
+		        if (deposit_cents >= total_price) then
+		          release_num_gums <= num_gums_wanted;
+		          return_deposit <= deposit_cents - total_price;
+		          deposit_cents<=0;  
+		          message<=0;
+		          display_total_deposit<=0;
+		          display_num_gums<=0;
+		          next_s<=output;
+		        end if;
+		      elsif (in_nickel = '1') then
+		        deposit_cents <= deposit_cents + 10;
+		        display_total_deposit <= deposit_cents + 10;
+		        if (deposit_cents >= total_price) then
+		          release_num_gums <= num_gums_wanted;
+		          return_deposit <= deposit_cents - total_price;
+		          deposit_cents<=0;  
+		          message<=0;
+		          display_total_deposit<=0;
+		          display_num_gums<=0;
+		          next_s<=output;
+		        end if;
+		        elsif (in_quarter = '1') then
+		        deposit_cents <= deposit_cents + 25;
+		        display_total_deposit <= deposit_cents + 25;
+		        if (deposit_cents >= total_price) then
+		          release_num_gums <= num_gums_wanted;
+		          return_deposit <= deposit_cents - total_price;
+		          deposit_cents<=0;  
+		          message<=0;
+		          display_total_deposit<=0;
+		          display_num_gums<=0;
+		          next_s<=output;
+		        end if;
+		      end if; 
+		    elsif (current_s = output) then
+		        message<=0;
 		        display_total_deposit<=0;
 		        display_num_gums<=0;
 		        return_deposit<=0;
@@ -51,11 +94,9 @@ begin
 		        release_num_gums<=0;
 		        num_gums_wanted <=0;
 		        deposit_cents<=0;
-		        release_s <= initial;
-		    elsif(num_gums_wanted > 0) then
-		      total_price  <= 20 * num_item; 
-		      if (current_s = reset) then
-		        message<=0;
+		        next_s <= initial;
+		     elsif (current_s = reset) then
+	          message<=0;
 		        display_total_deposit<=0;
 		        display_num_gums<=0;
 		        return_deposit<=deposit_cents;
@@ -63,36 +104,8 @@ begin
 		        return_dime<=0;
 		        num_gums_wanted <=0;
 		        deposit_cents<=0;
-		      elsif (next_s = finish) then
-		        release_num_gums <= num_gums_wanted;
-		        return_deposit <= deposit_cents - total_price;
-		        deposit_cents<=0;  
-		        message<=0;
-		        display_total_deposit<=0;
-		        display_num_gums<=0;
-		        release_s <= idle;
-		        num_gums_wanted<=0;
-		      elsif (in_dime='1') then
-		        deposit_cents <= deposit_cents + 5;
-		        display_total_deposit <= deposit_cents;
-		        if (deposit_cents >= 20) then
-		          release_num_gums <= num_gums_wanted;
-		          return_deposit <= deposit_cents - total_price;
-		          deposit_cents<=0;  
-		          message<=0;
-		          display_total_deposit<=0;
-		          display_num_gums<=0;
-		          release_s <= idle;
-		        end if;
-		      elsif (in_nickel = '1') then
-		        deposit_cents <= deposit_cents + 10;
-		        display_total_deposit <= deposit_cents;
-		        if (deposit_cents >= total_price) then
-		          release_s <= finish;
-		        end if;
-		      end if; 
+		        next_s <= initial;
 		     end if;
 		end process;
 		
 end arch_vending;
-
